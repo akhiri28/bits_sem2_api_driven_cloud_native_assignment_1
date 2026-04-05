4d91014edcb862fa52cb079ee325985adbaa2b80
akhiri28 <akhilesh.hari28@gmail.com>
2026-04-05 11:01:04 +0530

    updated the notebooks

--- MLOPS/Experimentation/main.py
+++ MLOPS/Experimentation/main.py
@@ -153,7 +153,7 @@ def main():
     y = data['Churn']
 
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # Evaluate and log to MLflow
     log_to_mlflow( X_train, X_test, y_train, y_test)
